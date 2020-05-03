32fab967eaf90ff3006286871813834559e4aca8
Eddie Estevez <eestevez@wisc.edu>
2020-05-03 18:05:36 -0500

    added number images and bitmaps into project_images c and h

--- project_images.h
+++ project_images.h
@@ -81,4 +81,56 @@ extern const uint8_t virus_detailBitmaps[];
 extern const uint8_t virus_detailWidthPixels;
 extern const uint8_t virus_detailHeightPixels;
 
+// Bitmap info for zero
+extern const uint8_t zeroBitmap[];
+extern const uint8_t zeroWidthPixels;
+extern const uint8_t zeroHeightPixels;
+
+// Bitmap info for one
+extern const uint8_t oneBitmap[];
+extern const uint8_t oneWidthPixels;
+extern const uint8_t oneHeightPixels;
+
+// Bitmap info for two
+extern const uint8_t twoBitmap[];
+extern const uint8_t twoWidthPixels;
+extern const uint8_t twoHeightPixels;
+
+// Bitmap info for three
+extern const uint8_t threeBitmap[];
+extern const uint8_t threeWidthPixels;
+extern const uint8_t threeHeightPixels;
+
+// Bitmap info for four
+extern const uint8_t fourBitmap[];
+extern const uint8_t fourWidthPixels;
+extern const uint8_t fourHeightPixels;
+
+// Bitmap info for five
+extern const uint8_t fiveBitmap[];
+extern const uint8_t fiveWidthPixels;
+extern const uint8_t fiveHeightPixels;
+
+// Bitmap info for six
+extern const uint8_t sixBitmap[];
+extern const uint8_t sixWidthPixels;
+extern const uint8_t sixHeightPixels;
+
+// Bitmap info for seven
+extern const uint8_t sevenBitmap[];
+extern const uint8_t sevenWidthPixels;
+extern const uint8_t sevenHeightPixels;
+
+// Bitmap info for eight
+extern const uint8_t eightBitmap[];
+extern const uint8_t eightWidthPixels;
+extern const uint8_t eightHeightPixels;
+
+// Bitmap info for nine
+extern const uint8_t nineBitmap[];
+extern const uint8_t nineWidthPixels;
+extern const uint8_t nineHeightPixels;
+
+
+
 #endif
